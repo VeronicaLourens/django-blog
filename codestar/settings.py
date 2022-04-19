96b028956774a2b93365ca4550c6fe2f88239417
Veronica Lourens <Veronica@lourens.cloud>
2022-04-19 08:40:21 +0000

    Build the admin site.

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-vl.herokuapp.com', 'locahost']
+ALLOWED_HOSTS = ['codestar-vl.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
